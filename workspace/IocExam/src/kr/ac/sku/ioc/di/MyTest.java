397b9bc9269045ffddc751b3ac66486e69e9a065
carami <carami@nate.com>
2022-08-18 15:54:21 +0900

    add ioc

---
+++ workspace/IocExam/src/kr/ac/sku/ioc/di/MyTest.java
@@ -0,0 +1,18 @@
+package kr.ac.sku.ioc.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		ApplicationContext factory 
+		= new ClassPathXmlApplicationContext("exam.xml");
+		
+		MyService my = factory.getBean(MyService.class);
+		
+		my.checkPrint();
+		System.out.println(my.getName());
+	}
+
+}
