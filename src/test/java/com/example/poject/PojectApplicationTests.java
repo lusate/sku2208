360d16069feea32c70b5c87e1025d19246ef2497
박상우 <rayrayr@naver.com>
2022-08-31 21:56:24 +0900

    Todo 리스트

---
+++ src/test/java/com/example/poject/PojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
